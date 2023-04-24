998a9719967b04f25ccd8c7a5f37a1c41003d877
Fedi Takeli <fedi.takeli1@gmail.com>
2023-04-24 23:28:51 +0100

    trace why tf the onTap callback does not fire

--- lib/quiz_game.dart
+++ lib/quiz_game.dart
@@ -14,64 +14,134 @@ class QuizGame extends StatefulWidget{
 class _QuizGameState extends State<QuizGame> {
   _QuizGameState(this._quizId);
   late final int _quizId;
-  late var _question;
+  List<Widget> _list = [];
+  PageController controller = PageController();
+  int _curr = 0;
   @override
   void initState() {
     super.initState();
-    _question = Question.create(_quizId, 0);
+    // _question = Question.create(_quizId, 0);
+    // to be changed
+    var lis = List.generate(1, (index) => Question.create(_quizId, index));
+    _list = lis.map((question) => buildQuizGame(question)).toList();
   }
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("quiz"),
+      ),
+      body: PageView(
+        allowImplicitScrolling: true,
+        scrollDirection: Axis.horizontal,
+        controller: controller,
+        onPageChanged: (num) {
+          setState(() {
+            _curr = num;
+          });
+        },
+        children: _list,
+      ),
+    );
+  }
+
+  Widget buildQuizGame(Future<Question> question){
     return FutureBuilder(
-        future: _question,
+        future: question,
         builder: (context, AsyncSnapshot snapshot){
           if (snapshot.hasData) {
-            return buildQuizGame(snapshot.data);
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 32,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text("question ${snapshot.data.questionIndex}"),
+                ),
+                const SizedBox(
+                  height: 32,
+                ),
+                const Divider(
+                  height: 10,
+                ),
+                optionsBuilder(options: snapshot.data.options)
+              ],
+            );
           } else {
             return const CircularProgressIndicator();
           }
         });
   }
-  
-  Widget buildQuizGame(Question question){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("quiz"),
-      ),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text("question no..."),
-          ),
-          SizedBox(
-            height: 32,
-          ),
-          Divider(
-            height: 10,
-          ),
-          PageView.builder(
-              itemBuilder: optionsBuilder(question),
-              controller: quizController,),
-        ],
+
+
+  Widget optionsBuilder( { required List<Option> options}){
+    bool lockOptions = false;
+    return Expanded(
+      child: Column(
+        children: options.map((option) => singleOptionBuilder(
+            option :option,
+            onClickedOption:  (option) {
+              // onclickedoption callback
+              if(lockOptions = false)
+              {
+                setState(() {
+                  option.isSelected = true;
+                  lockOptions = true;
+                });
+              }
+            },
+            lockOptions: lockOptions
+
+        )).toList(),
       ),
     );
   }
+
+  Widget singleOptionBuilder({required Option option, required ValueChanged<Option> onClickedOption, required bool lockOptions}){
+    var color = lockOptions ? changeBorderColor(option) : Colors.black;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () => onClickedOption(option),
+        child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(color: color)
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(option.optionText),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color changeBorderColor(Option option){
+    if(option.isRight) {
+      return Colors.green;
+    }
+    if(option.isSelected && !option.isRight){
+      return Colors.red;
+    }
+    return Colors.black;
+  }
+
 }
 
-Widget optionsBuilder(Question question){
-  return Column(
-    children: [
-      question.options.map((option) => singleOptionbuilder())
-    ],
-  )
-}
+
 
 class Question{
 
   late final String questionText;
-  late List<Option> options;
+  List<Option> options = [];
   late final int quizId;
   late final int questionIndex;
 
@@ -91,15 +161,11 @@ class Question{
 
     snapshot = await ref.child("answers").get();
     var dboptionlist = snapshot.value as List;
-    // initialize options with index
+    // initialize options with index map the options texts
     for(int i = 0; i<dboptionlist.length; i++){
-      question.options.add(Option._create(i));
+      question.options.add(Option(i, dboptionlist[i], i == correctQuestionIndex));
     }
-    // map the options texts
-    question.options.map((option) {
-      option.optionText = dboptionlist[option.optionIndex];
-      option.isRight = option.optionIndex == correctQuestionIndex;
-    });
+
     // Return the fully initialized object
     return question;
   }
@@ -107,18 +173,16 @@ class Question{
 
 class Option{
   final int optionIndex;
-  late final String optionText;
-  late final bool isRight;
+  final String optionText;
+  final bool isRight;
   bool isSelected = false;
 
   /// Private constructor
-  Option._create(this.optionIndex);
+  Option(this.optionIndex, this.optionText, this.isRight);
 
   /// Factory
-  static Future<Option> create(int questionIndex, DatabaseReference ref) async {
-    var option = Option._create(questionIndex);
-
-
-    return option;
-  }
+  // static Future<Option> create(int questionIndex, DatabaseReference ref) async {
+  //   var option = Option._create(questionIndex);
+  //   return option;
+  // }
 }
